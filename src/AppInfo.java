49ba37b0bd2cc90d766eeeca0eb13b817155de04
s35183 <s35183@pjwstk.edu.pl>
2026-03-17 10:51:50 +0100

    Add helper information about the application

---
+++ src/AppInfo.java
@@ -0,0 +1,8 @@
+public class AppInfo {
+    public static void printInfo() {
+        System.out.println("Application: pgo-git-start");
+        System.out.println("Version: 1.0");
+        System.out.println("Author: Student");
+        System.out.println("Purpose: Learning Git branches");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Git project!");
         System.out.println("Working with branches is fun!");
+        AppInfo.printInfo();
     }
 }
